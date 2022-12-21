228148b546e8b15bbac987024b88be8aca824eac
Ahmed Noubli <ahmeed68@hotmail.fr>
2022-12-21 13:02:36 +0100

    conference quality annual indicators repository

---
+++ labmanager-2/src/main/java/fr/ciadlab/labmanager/repository/conference/ConferenceQualityAnnualIndicatorsRepository.java
@@ -0,0 +1,10 @@
+package fr.ciadlab.labmanager.repository.conference;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import fr.ciadlab.labmanager.entities.conference.ConferenceQualityAnnualIndicators;
+
+
+public interface ConferenceQualityAnnualIndicatorsRepository extends JpaRepository<ConferenceQualityAnnualIndicators, Integer> {
+
+}
