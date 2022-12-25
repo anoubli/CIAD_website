93a941b5b0746bcb63c20cd01f7568f156bd5a75
Ahmed Noubli <ahmeed68@hotmail.fr>
2022-12-25 17:40:11 +0100

    Conference Editor view

--- labmanager-2/src/main/java/fr/ciadlab/labmanager/controller/view/conference/ConferenceViewController.java
+++ labmanager-2/src/main/java/fr/ciadlab/labmanager/controller/view/conference/ConferenceViewController.java
@@ -7,11 +7,14 @@ import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import fr.ciadlab.labmanager.configuration.Constants;
 import fr.ciadlab.labmanager.controller.view.AbstractViewController;
+import fr.ciadlab.labmanager.entities.conference.Conference;
+import fr.ciadlab.labmanager.entities.journal.Journal;
 import fr.ciadlab.labmanager.service.conference.ConferenceService;
 import fr.ciadlab.labmanager.service.journal.JournalService;
 
@@ -39,6 +42,40 @@ public class ConferenceViewController extends AbstractViewController{
 		initAdminTableButtons(modelAndView, endpoint(Constants.CONFERENCE_EDITING_ENDPOINT, "conference")); //$NON-NLS-1$
 		modelAndView.addObject("Conferences", this.conferenceService.getAllConferences()); //$NON-NLS-1$
 		modelAndView.addObject("currentYear", Integer.valueOf(LocalDate.now().getYear())); //$NON-NLS-1$
+		return modelAndView;
+	}
+	
+	/** Show the editor for a conference. This editor permits to create or to edit a conference.
+	 *
+	 * @param conference the identifier of the journal to edit. If it is {@code null}, the endpoint
+	 *     is dedicated to the creation of a conference.
+	 * @param username the name of the logged-in user.
+	 * @return the model-view object.
+	 */
+	@GetMapping(value = "/" + Constants.CONFERENCE_EDITING_ENDPOINT)
+	public ModelAndView showConferenceEditor(
+			@RequestParam(required = false) Integer conference,
+			@CookieValue(name = "labmanager-user-id", defaultValue = Constants.ANONYMOUS) String username) {
+		getLogger().info("Opening /" + Constants.CONFERENCE_EDITING_ENDPOINT + " by " + username + " for conference " + conference); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		ensureCredentials(username);
+		final ModelAndView modelAndView = new ModelAndView("conferenceEditor"); //$NON-NLS-1$
+		initModelViewWithInternalProperties(modelAndView);
+		//
+		final Conference conferenceObj;
+		if (conference != null && conference.intValue() != 0) {
+			conferenceObj = this.conferenceService.getConferenceById(conference.intValue());
+			if (conferenceObj == null) {
+				throw new IllegalArgumentException("Conference not found: " + conference); //$NON-NLS-1$
+			}
+		} else {
+			conferenceObj = null;
+		}
+		//
+		modelAndView.addObject("conference", conferenceObj); //$NON-NLS-1$
+		modelAndView.addObject("formActionUrl", rooted(Constants.CONFERENCE_SAVING_ENDPOINT)); //$NON-NLS-1$
+		modelAndView.addObject("formRedirectUrl", rooted(Constants.CONFERENCE_LIST_ENDPOINT)); //$NON-NLS-1$
+//		modelAndView.addObject("scimagoQIndex_imageUrl", JournalService.SCIMAGO_URL_PREFIX + "{0}"); //$NON-NLS-1$ //$NON-NLS-2$
+		//
 		return modelAndView;
 	}
 	
